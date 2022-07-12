f4c64a88120e8400d39f6926e647e6ee0a07ab7b
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-12 19:09:28 +0200

    Revert to 53507a0b43cab13f92517b7b75336162c44c19b5

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import { Searchbar } from './Searchbar';
+
+export class App extends Component {
+  // state = {
+  //   results: [],
+  // };
+
+  render() {
+    return (
+      <>
+        <h1>Test</h1>
+        <Searchbar />
+      </>
+    );
+  }
+}
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,37 +1,17 @@
 import { Component } from 'react';
 
 export class Searchbar extends Component {
-  state = {
-    searchQuery: '',
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state.searchQuery);
-
-    this.reset();
-  };
-
-  reset = () => {
-    this.setState({ searchQuery: '' });
-  };
-
-  handleChange = e => {
-    this.setState({ searchQuery: e.currentTarget.value });
-  };
-
   render() {
     return (
       <>
         <header className="searchbar">
-          <form className="form" onSubmit={this.handleSubmit}>
+          <form className="form">
             <input
               className="input"
               type="text"
               autoComplete="off"
               autoFocus
               placeholder="Search images and photos"
-              onChange={this.handleChange}
             />
             <button type="submit" className="button">
               <span className="button-label">Search</span>
