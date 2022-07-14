58551b4d29cfc4cf55a399a467262f1e60a4def9
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-14 15:18:44 +0200

    fixing search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
 import { Component } from 'react';
 import { Searchbar } from './components/Searchbar';
 
+import { fetchImagesWithQuery } from './services/api';
+
 export class App extends Component {
-  // state = {
-  //   results: [],
-  // };
+  state = {
+    hints: [],
+    searchQuery: '',
+  };
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevProps.searchQuery !== this.props.searchQuery) {
+  //     fetchImagesWithQuery()
+  //       .then(response => {
+  //         if (response) {
+  //           console.log('response: ', response);
+  //           return response;
+  //         }
+  //       })
+  //       .then(searchQuery => this.setState({ searchQuery: searchQuery }));
+  //   }
+  // }
+  componentDidMount() {
+    fetchImagesWithQuery('cat')
+      .then(response => {
+        if (response) {
+          console.log('response: ', response);
+          return response;
+        }
+      })
+      .then(searchQuery => this.setState({ searchQuery: searchQuery }));
+  }
+
+  handleFormSubmit = queryFromSearchbar => {
+    this.setState({ searchQuery: queryFromSearchbar, hits: [], page: 1 });
+  };
 
   render() {
     return (
       <>
         <h1>Test</h1>
-        <Searchbar />
+        <Searchbar onSubmit={this.handleFormSubmit} />
       </>
     );
   }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,17 +1,35 @@
 import { Component } from 'react';
 
 export class Searchbar extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(' handleSubmit is trigerred: ');
+    this.props.onSubmit(this.state.searchQuery);
+  };
+
+  handleChange(e) {
+    // this.setState({ searchQuery: e.target.value });
+    this.setState({ searchQuery: e.currentTarget.value });
+  }
+
   render() {
     return (
       <>
         <header className="searchbar">
-          <form className="form">
+          <form className="form" onSubmit={this.handleSubmit}>
             <input
               className="input"
               type="text"
               autoComplete="off"
               autoFocus
               placeholder="Search images and photos"
+              value={this.state.searchQuery}
+              // onChange={this.handleChange}
+              onChange={e => this.handleChange(e)}
             />
             <button type="submit" className="button">
               <span className="button-label">Search</span>
