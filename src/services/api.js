9ce140bec6975baff33339bc96baccd4265b9479
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-17 14:27:30 +0200

    increased per_page from 3 to 7

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = " https://pixabay.com/api/";
 const API_KEY = "25748459-63f23aee85add1030efa422f3"
 
-export const fetchImagesWithQuery = async (searchQuery, per_page = 3, currentPage = 1) => {
+export const fetchImagesWithQuery = async (searchQuery, per_page = 7, currentPage = 1) => {
     const response = await axios.get(
         `?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&per_page=${per_page}&page=${currentPage}`
     )
