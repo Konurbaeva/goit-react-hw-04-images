9f1f1ddf5e0804021e600eac28ef6e88dd82ad46
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-17 14:22:54 +0200

    added styled components to ImageGalleryItem

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,10 +1,22 @@
-// import { ImageGalleryItem } from './ImageGalleryItem';
-
+import styled from 'styled-components';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+
+const ImageGalleryStyled = styled.ul`
+  display: grid;
+  max-width: calc(100vw - 48px);
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-gap: 16px;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding: 0;
+  list-style: none;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 function ImageGallery({ images, openModal }) {
   return (
-    <ul>
+    <ImageGalleryStyled>
       {images.map(({ id, webformatURL, largeImageURL, tags }) => (
         <ImageGalleryItem
           key={id}
@@ -14,7 +26,7 @@ function ImageGallery({ images, openModal }) {
           largeImageURL={largeImageURL}
         />
       ))}
-    </ul>
+    </ImageGalleryStyled>
   );
 }
 
