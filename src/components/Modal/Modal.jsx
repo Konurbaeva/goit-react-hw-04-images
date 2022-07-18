fd5eb555ce60bf88174dad62567739def5dc39dc
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-18 19:43:40 +0200

    refactoring styled components

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,28 +1,6 @@
 import { Component } from 'react';
 import { createPortal } from 'react-dom';
-import styled from 'styled-components';
-
-const Backdrop = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  /*  */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ModalContent = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  max-width: calc(100vw - 48px);
-  max-height: calc(100vh - 24px);
-`;
+import { Backdrop, ModalContent } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
