bbbbea8245136c7bd30ce5c00ea66c6a988af490
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-18 22:03:18 +0200

    changed https://konurbaeva.github.io

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -15,8 +15,8 @@ export const SearchbarStyled = styled.header`
   padding-bottom: 12px;
   color: #fff;
   background-color: #3f51b5;
-  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
-    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+  /* box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12); */
 `;
 
 export const SearchForm = styled.form`
@@ -25,7 +25,7 @@ export const SearchForm = styled.form`
   width: 100%;
   max-width: 600px;
   background-color: #fff;
-  border-radius: 3px;
+  /* border-radius: 3px; */
   overflow: hidden;
 `;
 
