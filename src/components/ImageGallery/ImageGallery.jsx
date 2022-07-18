28ec88fafc27d056ac323cfe32588416ca03ab6a
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-18 19:29:11 +0200

    refactor and outsource ImageGallery.styled

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,18 +1,5 @@
-import styled from 'styled-components';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
-
-const ImageGalleryStyled = styled.ul`
-  display: grid;
-  max-width: calc(100vw - 48px);
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-  grid-gap: 16px;
-  margin-top: 0;
-  margin-bottom: 0;
-  padding: 0;
-  list-style: none;
-  margin-left: auto;
-  margin-right: auto;
-`;
+import { ImageGalleryStyled } from './ImageGallery.styled';
 
 function ImageGallery({ images, openModal }) {
   return (
