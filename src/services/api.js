32ea7d9a4ffadb69cc2ca1de858bc26717597284
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-20 17:43:34 +0200

    fixed axios get params

--- src/services/api.js
+++ src/services/api.js
@@ -1,28 +1,20 @@
 import axios from "axios";
+import settings from "./constants";
 
-// create a folder constants and put those there
-axios.defaults.baseURL = " https://pixabay.com/api/";
-const API_KEY = "25748459-63f23aee85add1030efa422f3"
-
+const { BASE_URL, API_KEY } = settings;
 
 export const fetchImagesWithQuery = async (searchQuery, per_page = 8, currentPage = 1) => {
-    const response = await axios.get(
-        `?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&per_page=${per_page}&page=${currentPage}`
+    const response = await axios.get(BASE_URL, {
+        params: {
+            key: API_KEY,
+            q: searchQuery,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            per_page: per_page,
+            page: currentPage,
+        }
+    }
     )
     return response.data.hits
 }
 
-// export const fetchImagesWithQuery = async (searchQuery, per_page = 7, currentPage = 1) => {
-//     const response = await axios.get(
-//         `?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&per_page=${per_page}&page=${currentPage}`
-//     )
-//     return response.data.hits
-// }
-
-// axios.get('/api', {
-//     params: {
-//         searchQuery: '',
-//         per_page: 7,
-//         currentPage: 1,
-//     },
-// });
---
+++ src/services/constants.js
@@ -0,0 +1,7 @@
+const settings = {
+    API_KEY: "25748459-63f23aee85add1030efa422f3",
+    BASE_URL: "https://pixabay.com/api/"
+
+}
+
+export default settings;
